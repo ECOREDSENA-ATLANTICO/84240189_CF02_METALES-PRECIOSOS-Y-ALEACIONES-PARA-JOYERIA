1e5a9b6eee9784f239580ee6aea9c9609aaa559f
AndresDiazPinto <andresvis565@gmail.com>
2024-12-18 15:01:09 -0500

    Update global.js

--- src/config/global.js
+++ src/config/global.js
@@ -109,7 +109,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/CF02_84240189_DU.pdf',
       },
       {
         icono: 'fas fa-download',
